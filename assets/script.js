26e71bf206878c63b710c1d571570213ac08953d
Rajendran Giabilan <giabilan.r@gmail.com>
2023-05-24 14:20:27 +0200

    slides text errors updated

--- assets/script.js
+++ assets/script.js
@@ -10,7 +10,7 @@ const slides = [
   },
   {
     image: "slide3.jpg",
-    tagLine: "Grand choix de coulesurs <span>de CMJN aux pantones</span>",
+    tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
   },
   {
     image: "slide4.png",
@@ -21,8 +21,8 @@ const slides = [
 // Sélection de tous les éléments avec la classe "arrow"
 const arrows = document.querySelectorAll(".arrow");
 
-// Déclaration d'une variable "cursor" pour suivre la position actuelle du slide, i,itialisé à 1 
-let cursor = 1; 
+// Déclaration d'une variable "cursor" pour suivre la position actuelle du slide, i,itialisé à 1
+let cursor = 1;
 
 // Boucle sur *chaque* élément "arrow"
 arrows.forEach(function (arrow) {
@@ -33,30 +33,29 @@ arrows.forEach(function (arrow) {
 
     // Si arrow à la classe "arrow_right"
     if (arrow.classList.contains("arrow_right")) {
-      // Incrémentation de cursor de 1
-      cursor = cursor + 1;
-      // Si cursor atteint 5, il est réinitialisé à 1
-      if (cursor === 5) { 
+      cursor++;
+      if (cursor === 5) {
         cursor = 1; // Première image
-      } 
-    } else { // Sinon arrow à la classe "arrow_left"
-      // Décrémentation de cursor de 1
-      cursor = cursor - 1;
-      // Si cursor atteint 0, il est réinitialisé à 4
-      if (cursor === 0) {  
+      }
+    } else {
+      // Sinon arrow à la classe "arrow_left"
+      cursor--;
+      if (cursor === 0) {
         cursor = 4; // Dernière image
       }
-    } 
+    }
 
     // Ajout de la classe "dot_selected" à l'élément dot correspondant à la position actuelle du slide
     document.getElementById("dot" + cursor).className = "dot dot_selected";
 
     // cursor - 1 car le tableau commence à l'index 0 pour les images
     let currentImage = slides[cursor - 1].image;
-    document.querySelector(".banner-img").src = `./assets/images/slideshow/${currentImage}`;
+    document.querySelector(
+      ".banner-img"
+    ).src = `./assets/images/slideshow/${currentImage}`;
 
     // cursor - 1 car le tableau commence à l'index 0 pour les taglines (textes)
     let currentTagline = slides[cursor - 1].tagLine;
     document.getElementById("tagline").innerHTML = currentTagline;
-  }); 
+  });
 });
